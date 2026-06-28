326909c6efb42d4aecf1934b002d55f8653df2f3
CKAJIA <badstealth@gmail.com>
2026-06-28 15:26:05 +0500

    Add trade files

---
+++ client/ui/trade_callbacks.lua
@@ -0,0 +1,56 @@
+RegisterNUICallback('AddTradeItem', function(data, cb)
+	if not data or not data.item or not data.amount or not data.token or not ValidateInventoryCbToken(data.token) then
+        cb({ ok = false })
+        return
+    end
+
+	local success = lib.callback.await('rsg-inventory:server:addTradeItem', false, data.tradeId, data.item, data.amount)
+    cb({ ok = success == true })
+end)
+
+RegisterNUICallback('RemoveTradeItem', function(data, cb)
+    if not data or not data.tradeId or not data.tradeSlot or not data.token or not ValidateInventoryCbToken(data.token) then
+        cb(false)
+        return
+    end
+	
+    local tradeId = data.tradeId
+    local tradeSlot = tonumber(data.tradeSlot)
+    local targetSlot = tonumber(data.targetSlot)
+    local amount = tonumber(data.amount) or 1
+
+    if not tradeSlot or not targetSlot or amount < 1 then
+        cb(false)
+        return
+    end
+
+    local success = lib.callback.await('rsg-inventory:server:removeTradeItem', false, tradeId, tradeSlot, targetSlot, amount)
+    cb(success == true)
+end)
+
+RegisterNUICallback('ConfirmTrade', function(data, cb)
+    if not data or not data.tradeId or not data.token or not ValidateInventoryCbToken(data.token) then
+        cb('ok')
+        return
+    end
+    TriggerServerEvent('rsg-inventory:server:confirmTrade', data.tradeId)
+    cb('ok')
+end)
+
+RegisterNUICallback('CancelTrade', function(data, cb)
+    if not data or not data.tradeId or not data.token or not ValidateInventoryCbToken(data.token) then
+        cb('ok')
+        return
+    end
+    TriggerServerEvent('rsg-inventory:server:cancelTrade', data.tradeId)
+    cb('ok')
+end)
+
+RegisterNUICallback('InitiateTrade', function(data, cb)
+    if not data or not data.targetId or not data.token or not ValidateInventoryCbToken(data.token) then
+        cb('ok')
+        return
+    end
+    TriggerServerEvent('rsg-inventory:server:initiateTrade', data.targetId)
+    cb('ok')
+end)
